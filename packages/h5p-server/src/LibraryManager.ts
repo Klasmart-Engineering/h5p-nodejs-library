7a6a2b735f9a5d6202ec3a6a32978b1905feb1c0
cabauman <cab452005@gmail.com>
2022-01-03 11:27:04 +0900

    feat(h5p-server): load language based on various locale formats
    https://bitbucket.org/calmisland/kidsloop-h5p-library/pull-requests/25

--- packages/h5p-server/src/LibraryManager.ts
+++ packages/h5p-server/src/LibraryManager.ts
@@ -916,9 +916,9 @@ export default class LibraryManager {
                 library
             )}`
         );
-        const languageFileAsString = await this.libraryStorage.getFileAsString(
+        const languageFileAsString = await this.getLanguageFileAsString(
             library,
-            `language/${language}.json`
+            language
         );
         // If the implementation has specified one, we use a hook to alter
         // the language files to match the structure of the altered
@@ -933,6 +933,75 @@ export default class LibraryManager {
                 )
             });
         }
+        return languageFileAsString;
+    }
+
+    private async getLanguageFileAsString(
+        library: ILibraryName,
+        language: string
+    ): Promise<string> {
+        const languageFileFormats = [
+            language, // whatever format the cookie is in
+            (() => {
+                // xx_XX
+                const [languageCode, countryCode] = language
+                    .replace(`-`, `_`)
+                    .split(`_`);
+                if (!languageCode || !countryCode) return;
+                return `${languageCode.toLowerCase()}_${countryCode.toUpperCase()}`;
+            })(),
+            (() => {
+                // xx-XX
+                const [languageCode, countryCode] = language
+                    .replace(`_`, `-`)
+                    .split(`-`);
+                if (!languageCode || !countryCode) return;
+                return `${languageCode.toLowerCase()}-${countryCode.toUpperCase()}`;
+            })(),
+            (() => {
+                // xx_xx
+                const [languageCode, countryCode] = language
+                    .toLowerCase()
+                    .replace(`-`, `_`)
+                    .split(`_`);
+                if (!languageCode || !countryCode) return;
+                return `${languageCode}_${countryCode}`;
+            })(),
+            (() => {
+                // xx-xx
+                const [languageCode, countryCode] = language
+                    .toLowerCase()
+                    .replace(`_`, `-`)
+                    .split(`-`);
+                if (!languageCode || !countryCode) return;
+                return `${languageCode}-${countryCode}`;
+            })(),
+            (() => {
+                // xx
+                const [languageCode] = language.toLowerCase().split(/[_-]/);
+                if (!languageCode) return;
+                return `${languageCode}`;
+            })()
+        ].filter((fileFormat): fileFormat is string => !!fileFormat);
+
+        let languageFile = null;
+        for (const fileFormat of languageFileFormats) {
+            // eslint-disable-next-line no-await-in-loop
+            const fileExists = await this.libraryStorage.fileExists(
+                library,
+                `language/${fileFormat}.json`
+            );
+            if (fileExists) {
+                languageFile = `language/${fileFormat}.json`;
+                break;
+            }
+        }
+
+        const languageFileAsString = await this.libraryStorage.getFileAsString(
+            library,
+            languageFile
+        );
+
         return languageFileAsString;
     }
 }
